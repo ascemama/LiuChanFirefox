3ced8beae82f522685da054c5529842234168fb5
Antoine Scemama <AScemama@brainloop.com>
2019-10-24 12:00:30 +0800

    export first commit

--- js/vocabLists.js
+++ js/vocabLists.js
@@ -1,66 +1,94 @@
 'use strict';
-class vocabLists{
-constructor(){
+class vocabLists {
+  constructor() {
 
     browser.contextMenus.create({
-        id: "addToVocabList",
-        title:"Add Item to List",
-        contexts: ["all"]
-      }, this.onContextMenuCreated) 
+      id: "addToVocabList",
+      title: "Add Item to List",
+      contexts: ["all"]
+    }, this.onContextMenuCreated)
 
-      browser.contextMenus.create({
-        id: "deleteVocabList",
-        title:"Delete vocabuliary list",
-        contexts: ["all"]
-      }, this.onContextMenuCreated) 
+    browser.contextMenus.create({
+      id: "deleteVocabList",
+      title: "Delete vocabuliary list",
+      contexts: ["all"]
+    }, this.onContextMenuCreated)
 
 
-      browser.contextMenus.onClicked.addListener(function(info, tab) { 
-        switch (info.menuItemId) {
-          case "addToVocabList":
-            let items=browser.storage.local.get();
-            items.then(res=> {
+    browser.contextMenus.create({
+      id: "exportVocabList",
+      title: "Export vocabuliary list",
+      contexts: ["all"]
+    }, this.onContextMenuCreated)
+
+    browser.contextMenus.onClicked.addListener(function (info, tab) {
+      switch (info.menuItemId) {
+        case "addToVocabList":
+          let items = browser.storage.local.get();
+          items.then(res => {
             //list is empty
-              if(res.list === undefined){
-                var d=new Date()
+            if (res.list === undefined) {
+              var d = new Date()
 
-                var item={ list:"//"+d.toDateString()+"\n"+info.selectionText+ "\n"};
-              }
-              else{
-                item={list:res.list+info.selectionText+"\n"};
-              }
+              var item = { list: "//" + d.toDateString() + '\r\n' + info.selectionText + '\r\n' };
+            }
+            else {
+              item = { list: res.list + info.selectionText + '\r\n' };
+            }
             //re-write
-              browser.storage.local.set(item);
-              items=browser.storage.local.get();
-              items.then(res=>{console.log(JSON.stringify(res))});
-            });
-            break;
-            case "deleteVocabList":
-              let  itemsToBeDeleted=browser.storage.local.get();
-                itemsToBeDeleted.then(res=> {
-                //list is empty
-                  if(res.list !== undefined){
-                    browser.storage.local.remove("list")
-                  }});
-                break;
+            browser.storage.local.set(item).then(res => {
+              vocabLists.showStorageContent();
+            })
+          });
+          break;
+        case "deleteVocabList":
+          let itemsToBeDeleted = browser.storage.local.get();
+          itemsToBeDeleted.then(res => {
+            //list is empty
+            if (res.list !== undefined) {
+              browser.storage.local.remove("list")
+            }
+            vocabLists.showStorageContent();
+          });
+          break;
+        case "exportVocabList":
+          let itemsToBeExported = browser.storage.local.get();
+          itemsToBeExported.then(res => {
+            //list is empty, no download
+            if (res.list !== undefined) {
+              let objURL = URL.createObjectURL(new Blob([JSON.stringify(res.list, null, 2).replace(/"/g,"")], { type: 'application/json' }));
+              browser.downloads.download({ url: objURL });
+            }
+            else{
+              console.log("no list to export")
+            }
+            vocabLists.showStorageContent();
+          });
 
-        }
-      })
-}
-updateContextMenu(m){
+
+
+      }
+    })
+  }
+  updateContextMenu(m) {
     browser.contextMenus.update(
-        "addToVocabList",               // integer or string
-        {title: "Add "+ m + " to list"} // object
-      )
-}
+      "addToVocabList",               // integer or string
+      { title: "Add " + m + " to list" } // object
+    )
+  }
 
 
-onContextMenusCreated() {
+  onContextMenusCreated() {
     if (browser.runtime.lastError) {
       console.log(`Error: ${browser.runtime.lastError}`);
     } else {
       console.log("Item created successfully");
     }
+  }
+
+  static showStorageContent() {
+    let items = browser.storage.local.get();
+    items.then(res => { console.log(JSON.stringify(res)) });
   }
 
 }
